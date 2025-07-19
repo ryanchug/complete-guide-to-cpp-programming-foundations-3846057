235991413153f857d0708eeff72954197f8e58e8
Rayaan Chughtai <65190920+ryanchug@users.noreply.github.com>
2025-07-19 15:52:24 +0000

    commit practice

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -8,7 +8,7 @@ int main(){
     float num_1, num_2, result;
 
     std::cout << "Enter number 1: " << std::flush;
-    std::cin >> num_1;
+    std::cin >> num_1; 
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
     
