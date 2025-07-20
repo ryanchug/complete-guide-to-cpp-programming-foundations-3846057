1ee50462b8330bb47a67691d78c24b3fc3d982dd
Rayaan Chughtai <65190920+ryanchug@users.noreply.github.com>
2025-07-20 13:40:25 +0000

    Ch1 2

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -3,9 +3,16 @@
 // Hello World, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
 
 int main(){
+    std::string name;
     std::cout << "Hi There!" << std::endl;
+    std::cout << "What's your name: " << std::flush;
+
+    std::cin >> name;
+
+    std::cout << "Hello there, " << name << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return 0;
